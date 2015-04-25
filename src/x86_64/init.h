26010463bb27194e0e0a5ceba6a5d46d95bcc75c
Kamil Rytarowski <n54@gmx.com>
2015-04-25 23:32:40 +0000

    fix fir netbsd

--- src/x86_64/init.h
+++ src/x86_64/init.h
@@ -32,9 +32,13 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 # define REG_INIT_LOC(c, rlc, ruc) \
     DWARF_LOC ((unw_word_t) &c->uc->uc_mcontext.gregs[REG_ ## ruc], 0)
 
-#elif defined UNW_LOCAL_ONLY && (defined(__FreeBSD__) || defined(__NetBSD__))
+#elif defined UNW_LOCAL_ONLY && defined(__FreeBSD__)
 # define REG_INIT_LOC(c, rlc, ruc) \
     DWARF_LOC ((unw_word_t) &c->uc->uc_mcontext.mc_ ## rlc, 0)
+
+#elif defined UNW_LOCAL_ONLY && defined(__NetBSD__)
+# define REG_INIT_LOC(c, rlc, ruc) \
+    DWARF_LOC ((unw_word_t) &c->uc->uc_mcontext.__gregs[_REG_ ## ruc, 0)
 
 #else
 # define REG_INIT_LOC(c, rlc, ruc) \
